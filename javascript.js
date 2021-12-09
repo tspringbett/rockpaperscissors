c3e33b0e97e0655f0327b615f1023605ecb69933
tspringbett <93589216+tspringbett@users.noreply.github.com>
2021-12-09 13:36:21 +0000

    The game now runs, although ont completely perfectly, it runs 5 times and outputs an overall winner. I need to work on the result it give out after each round. I have used a loop function to run the game 5 times round.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
+let round = 0;
+let computerScore = 0;
+let playerScore = 0;
+let tie = 0;
+
+
+
 
 // function used to select the weapon the computer will use
 function computerPlay() {
@@ -8,31 +15,62 @@ function computerPlay() {
 const playerSelection = prompt("Pick your weapon. Rock, Paper or Scissors").toLowerCase();
 const computerSelection = computerPlay();
 
-function playRound(playerSelection, computerSelection) {
-    //paper round
-    if (playerSelection === "paper" && computerSelection === "paper") {
-        return "its a tie";
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You Win! Paper beats Rock!";
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You lose! Sciccors beats paper!";
-    } 
-    //rock round
+
+    function playRound(playerSelection, computerSelection) {
+        const score = {playerScore : 0, computerScore : 0};
+        //paper round
+        if (playerSelection === "paper" && computerSelection === "paper") {
+            tie++;
+            alert("its a tie");
+        }  else if (playerSelection === "paper" && computerSelection === "rock") {
+            playerScore++;
+            alert("You Win! Paper beats Rock!");
+        }  else if (playerSelection === "paper" && computerSelection === "scissors") {
+            computerScore++;
+            alert("You lose! Sciccors beats paper!");
+        } 
+        //rock round
         else if (playerSelection === "rock" && computerSelection === "rock") {
-        return "its a tie";
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You Lose! Paper beats Rock!";
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You Win! Rock beats scissors!";
-    }
-    //scissors round
+            tie++;    
+            alert("its a tie");
+        }  else if (playerSelection === "rock" && computerSelection === "paper")  { 
+            computerScore++;
+            alert("You Lose! Paper beats Rock!");
+        }  else if (playerSelection === "rock" && computerSelection === "scissors") {
+            playerScore++;
+            alert("You Win! Rock beats scissors!");
+        }
+        //scissors round
         else if (playerSelection === "scissors" && computerSelection === "scissors") {
-        return "its a tie";
-    }   else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You Lose! Rock beats Scissors!";
-    }   else (playerSelection === "Sciccors" && computerSelection === "paper")
-        return "You Win! Scissors beat paper!";
+            tie++;
+            alert("its a tie");
+        }   else if (playerSelection === "scissors" && computerSelection === "rock") {
+            computerScore++;
+            alert("You Lose! Rock beats Scissors!");
+        }   else if (playerSelection === "Sciccors" && computerSelection === "paper") {
+            playerScore++;
+            alert("You Win! Scissors beat paper!");
+    } 
 }
 
+function result(){
+    if  ((tie > playerScore) && (tie > computerScore)){
+        console.log("Its a tie! Nobody wins.")
+    } 
+    else if (playerScore > computerScore){
+        console.log("You win! Never though this would happend...")
+    } 
+    else {
+        console.log("HAHAHA, You loser!!")
+    }
+}
 
-    console.log(playRound(playerSelection, computerSelection));
+for (let round = 0; round < 4; round++) {
+    let playerSelection = prompt("Rock, Paper or Scissors?");
+    let computerSelection = computerPlay();
+  
+    playRound(playerSelection, computerSelection)
+
+}
+   
+result();
